f958c7d8b1ac14d9dd3e06ef1c42856c2d5c4c1a
David Jacobs <david.jacobs@tangramflex.com>
2018-11-26 16:14:19 -0500

    Cleanup main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,22 +2,23 @@ package example_1
 
 import models._
 import calculations._
+import scala.math.Pi
 
 /**
 *   Main is where the application will enter
 */
 object Main extends App {
 
+    // Create the shapes
     val s = Square(Red, 2)
-    SquareCalc(s).displayCalculations()
-
-    val r = Rectangle(Blue, 2, 4)
-    RectangleCalc(r).displayCalculations()
-
+    val r = Rectangle(Blue, 2, 3)
     val t = Triangle(Yellow, 8, 7, 3)
-    TriangleCalc(t).displayCalculations()
-
     val c = Circle(Red, 3)
+
+    // Display the shapes calculations
+    SquareCalc(s).displayCalculations()
+    RectangleCalc(r).displayCalculations()
+    TriangleCalc(t).displayCalculations()
     CircleCalc(c).displayCalculations()
 
 }
