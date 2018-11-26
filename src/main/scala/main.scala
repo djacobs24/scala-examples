0253f6cc4f8742893f73e470cb4cc3753b28082e
David Jacobs <david.jacobs@tangramflex.com>
2018-11-26 11:02:15 -0500

    Get the area for all shapes in main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,19 @@
-package scala
+package example_1
+
+import models._
+import calculations._
 
 object Main extends App {
 
-    println("Hello world!")
+    val s = Square("square", 2)
+    val sArea = SquareCalc(s).getArea()
+    println(sArea)
 
+    val r = Rectangle("rectangle", 2, 4)
+    val rArea = RectangleCalc(r).getArea()
+    println(rArea)
+
+    val t = Triangle("triangle", 6, 3)
+    val tArea = TriangleCalc(t).getArea()
+    println(tArea)
 }
