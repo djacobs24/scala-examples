75ee404388b01882094d712b949ea55949b9361c
David Jacobs <david.jacobs@tangramflex.com>
2018-11-28 11:57:53 -0500

    Update example 1 tests

--- src/test/scala/example_1/example_1_tests.scala
+++ src/test/scala/example_1/example_1_tests.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 import models._
 import calculations._
 
-class calculationsTest extends FlatSpec with Matchers {
+class Example1Tests extends FlatSpec with Matchers {
 
     /**
      *  Defining the shapes
@@ -30,56 +30,56 @@ class calculationsTest extends FlatSpec with Matchers {
     /**
      *  A square with sides of 2 should have an area of 4   
      */
-    "Square area" should "equal 4" in {
+    "Square area with sides of 2" should "equal 4" in {
         squareArea should be (4)
     }
 
     /**
      *  A rectangle with sides of 2 and 3 should have an area of 6   
      */
-    "Rectangle area" should "equal 6" in {
+    "Rectangle area with sides of 2 and 3" should "equal 6" in {
         rectangleArea should be (6)
     }
 
     /**
      *  A triangle with sides of 8, 7, and 3 should have an area of 8   
      */
-    "Triangle area" should "equal 10.39" in {
+    "Triangle area with sides of 8, 7, and 3" should "equal 10.39" in {
         BigDecimal(triangleArea).setScale(2, roundingMode).toDouble should be (10.39)
     }
 
     /**
      *  A circle with a radius of 4 should have an area of 28.27
      */
-    "Circle area" should "equal 28.27" in {
+    "Circle area with a radius of 4" should "equal 28.27" in {
         BigDecimal(circleArea).setScale(2, roundingMode).toDouble should be (28.27)
     }
 
     /**
      *  A square with sides of 2 should have a perimeter of 8   
      */
-    "Square perimeter" should "equal 8" in {
+    "Square perimeter with sides of 2" should "equal 8" in {
         squarePerimeter should be (8)
     }
 
     /**
      *  A rectangle with sides of 2 and 3 should have a perimeter of 10 
      */
-    "Rectangle perimeter" should "equal 10" in {
+    "Rectangle perimeter with sides of 2 and 3" should "equal 10" in {
         rectanglePerimeter should be (10)
     }
 
     /**
      *  A triangle with sides of 8, 7, and 3 should have a perimeter of 18  
      */
-    "Triangle perimeter" should "equal 18" in {
+    "Triangle perimeter with sides of 8, 7, and 3" should "equal 18" in {
         trianglePerimeter should be (18)
     }
 
     /**
      *  A circle with a radius of 4 should have a perimeter of 18.85
      */
-    "Circle perimeter" should "equal 18.85" in {
+    "Circle perimeter with a radius of 4" should "equal 18.85" in {
         BigDecimal(circlePerimeter).setScale(2, roundingMode).toDouble should be (18.85)
     }
 
