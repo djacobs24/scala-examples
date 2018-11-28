53a33c6756b1dda2c4bf310b1e96299c4d1ca710
David Jacobs <david.jacobs@tangramflex.com>
2018-11-28 11:31:54 -0500

    Comment out example 1 main for work on example 2

--- src/main/scala/example_1/main.scala
+++ src/main/scala/example_1/main.scala
@@ -9,18 +9,18 @@ import scala.math.Pi
  */
 object Main {
 
-    def main(args: Array[String]): Unit = {
-        // Create the shapes
-        val s = Square(Red, 2)
-        val r = Rectangle(Blue, 2, 3)
-        val t = Triangle(Yellow, 8, 7, 3)
-        val c = Circle(Red, 3)
+    // def main(args: Array[String]): Unit = {
+    //     // Create the shapes
+    //     val s = Square(Red, 2)
+    //     val r = Rectangle(Blue, 2, 3)
+    //     val t = Triangle(Yellow, 8, 7, 3)
+    //     val c = Circle(Red, 3)
 
-        // Display the calculations
-        SquareCalc(s).displayCalculations
-        RectangleCalc(r).displayCalculations
-        TriangleCalc(t).displayCalculations
-        CircleCalc(c).displayCalculations
-    }
+    //     // Display the calculations
+    //     SquareCalc(s).displayCalculations
+    //     RectangleCalc(r).displayCalculations
+    //     TriangleCalc(t).displayCalculations
+    //     CircleCalc(c).displayCalculations
+    // }
 
 }
