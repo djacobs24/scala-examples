8f9b2b9b7cf7875ec1de0266dfd0c76497728063
David Jacobs <david.jacobs@tangramflex.com>
2018-11-29 13:18:50 -0500

    Uncomment example 1 & 2 main

--- src/main/scala/example_2/main.scala
+++ src/main/scala/example_2/main.scala
@@ -8,24 +8,26 @@ import models._
  */
 object Main {
 
-    // def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
         /**
          *  Polymorphic Methods
          */
-         // Display the index
-        // println(PolymorphicMethod.findFirst(Array(0, 1, 2, 3), (x: Int) => x == 2))
+
+        // Display the index
+        println(PolymorphicMethod.findFirst(Array(0, 1, 2, 3), (x: Int) => x == 2))
 
         /**
          *  Pattern Matching
          */
-         // Create the notifications
-        // val email = Email("Susan", "Meeting on Tuesday", "Are we still meeting?")
-        // val sms = SMS("419-262-4600", "Are you there?")
-        // val voiceRecording = VoiceRecording("Tom", "voicerecording.org/id/12345")
+
+        // Create the notifications
+        val email = Email("Susan", "Meeting on Tuesday", "Are we still meeting?")
+        val sms = SMS("419-262-4600", "Are you there?")
+        val voiceRecording = VoiceRecording("Tom", "voicerecording.org/id/12345")
         // Display the notifications
-        // println(PatternMatching.showNotification(email))
-        // println(PatternMatching.showNotification(sms))
-        // println(PatternMatching.showNotification(voiceRecording))
-    // }
+        println(PatternMatching.showNotification(email))
+        println(PatternMatching.showNotification(sms))
+        println(PatternMatching.showNotification(voiceRecording))
+    }
     
 }
