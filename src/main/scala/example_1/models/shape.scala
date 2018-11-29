420c4e95365bfb94ee4275ba874f63e3c89d986f
David Jacobs <david.jacobs@tangramflex.com>
2018-11-29 14:03:27 -0500

    Combine example 1 shapes into single scala file

--- src/main/scala/example_1/models/shape.scala
+++ src/main/scala/example_1/models/shape.scala
@@ -3,7 +3,46 @@ package models
 /**
  *  Shape is the base of all "shapes"
  */
-trait Shape {
+sealed trait Shape {
     val color: PrimeColor
     val isPolygon: Boolean
 }
+
+/**
+ *  A representation of a square object
+ */
+case class Square (
+    color: PrimeColor,
+    side: Float,
+    isPolygon: Boolean = true
+) extends Shape
+
+/**
+ *  A representation of a rectangle object
+ */
+case class Rectangle (
+    color: PrimeColor,
+    length: Float,
+    width: Float,
+    isPolygon: Boolean = true
+) extends Shape
+
+/**
+ *  A representation of a triangle object
+ */
+case class Triangle (
+    color: PrimeColor,
+    sideOne: Float,
+    sideTwo: Float,
+    sideThree: Float,
+    isPolygon: Boolean = true
+) extends Shape
+
+/**
+ *  A representation of a circle object
+ */
+case class Circle (
+    color: PrimeColor,
+    radius: Float,
+    isPolygon: Boolean = false
+) extends Shape
