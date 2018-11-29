05d287655b16c0ba1cfd48a9b879771e38955000
David Jacobs <david.jacobs@tangramflex.com>
2018-11-29 10:34:42 -0500

    Add data for example 3

---
+++ src/main/scala/example_3/data/data.scala
@@ -0,0 +1,13 @@
+package data
+
+object Data {
+
+    val list1 = List()
+    val list2 = List(1)
+    val list3 = List(1,2)
+    val list4 = List(1,2,3)
+    val list5 = List("a")
+    val list6 = List("a","b")
+    val list7 = List("a","b","c")
+
+}
