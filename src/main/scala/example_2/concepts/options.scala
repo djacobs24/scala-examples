46319e580d76ba1e3ffa94e4daf08b646a66108b
David Jacobs <david.jacobs@tangramflex.com>
2018-12-03 15:59:20 -0500

    Add options to example 2

---
+++ src/main/scala/example_2/concepts/options.scala
@@ -0,0 +1,21 @@
+package concepts
+
+object Options {
+
+    sealed trait Option[+A]
+    case class Some[+A](get: A) extends Option[A]
+    case object None extends Option[Nothing]
+
+    // mean calculates the mean of a list
+    def mean(xs: Seq[Double]): Option[Double] =
+        if (xs.isEmpty) None
+        else Some(xs.sum / xs.length)
+
+    // displayOptionsData displays the data
+    def displayOptionsData() {
+        println(mean(Seq(4,6,8)))
+        println(mean(Seq(2.2,6.1,4.9)))
+        println(mean(Seq()))
+    }
+
+}
